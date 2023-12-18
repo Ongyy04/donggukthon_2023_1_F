cc8bc56273f14c5d5bccb9e40276912a977404fe
kyujenius <hkj020607@gmail.com>
2023-12-18 19:45:50 +0900

    ADD Carousel->moudle/ Vote, Waiting Pages

--- src/pages/Waiting.js
+++ src/pages/Waiting.js
@@ -3,29 +3,34 @@ import styles from "./Home.module.scss";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
-function Home() {
+function Waiting() {
   const [imageLoaded, setImageLoaded] = useState(true);
   const navigate = useNavigate();
   const handleImageLoad = () => {
     setImageLoaded(true);
   };
-  const hanldMakeRoomButton = () => {
-    navigate("/groupMake");
+  const hanedleClickButton = () => {
+    navigate("/home");
   };
+  const captions = "투표가 \n 완료되었습니다.";
+  const miniCaptions =
+    "모든 사람의 투표가 완료되면 \n 결과를 확인할 수 있습니다.";
   return (
     <div className={styles.container}>
       {imageLoaded ? (
         <div className={styles.ImgandObjectContainer}>
           <div className={styles.imageContainer}>
             <img
-              src="/assets/snow-character.png"
+              src="/assets/party.png"
               alt="Decorative Snowflake"
               onLoad={handleImageLoad}
             />
           </div>
           <div className={styles.buttonsContainer}>
-            <Button text={"그룹 만들기"} onClick={hanldMakeRoomButton} />
-            <Button text={"내 그룹 들어가기"} />
+            <h1>{captions}</h1>
+            <br></br>
+            <p>{miniCaptions}</p>
+            <Button text={"다음 질문 보기"} onClick={hanedleClickButton} />
           </div>
         </div>
       ) : (
@@ -36,4 +41,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Waiting;
