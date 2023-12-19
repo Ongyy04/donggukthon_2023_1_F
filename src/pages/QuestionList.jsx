d96a801769a33efe53afb5f109700f8d3dc6e428
Kong-E <kng001016@dgu.ac.kr>
2023-12-19 19:28:36 +0900

    Add questions query

--- src/pages/QuestionList.jsx
+++ src/pages/QuestionList.jsx
@@ -1,43 +1,46 @@
-import React, { useState } from "react";
-import styles from "./Home.module.scss";
-import Button from "../components/Button";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import styles from './Home.module.scss';
+import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getQuestions } from '../api/question';
+
+const DUMMY_QUESTIONS = [
+  { questionId: 1, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 2, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 3, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 4, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 5, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 6, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 7, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 8, question: '당신의 취미는 무엇인가요?' },
+  { questionId: 9, question: '당신의 취미는 무엇인가요?' },
+  //fetching으로 데이터를 받아와야 함.
+];
+
 function QuestionList() {
   const [imageLoaded, setImageLoaded] = useState(true);
   const navigator = useNavigate();
+  const {
+    data: questionsData,
+    error,
+    isLoading,
+  } = useQuery(['questions'], () => getQuestions(), {
+    select: questionsData => questionsData.data.questionList,
+  });
 
-  const DUMMY_QUESTIONS = [
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    "질문 ------------------1",
-    //fetching으로 데이터를 받아와야 함.
-  ];
-  const hanedleClickNameButton = (e) => {
-    console.log("당신이 누른 질문은", e.target.textContent, "입니다.");
-    navigator("/guess");
+  const hanedleClickNameButton = e => {
+    console.log('당신이 누른 질문은', e.target.textContent, '입니다.');
+    navigator('/guess');
   };
   return (
     <div className={styles.container}>
       {imageLoaded ? (
         <div className={styles.ImgandObjectContainer}>
-          <div className={styles.imageContainer}>
-            <h1>원하는 주제를 Pick! 하세요!</h1>
-          </div>
+          <h1>원하는 주제를 Pick! 하세요!</h1>
           <div className={styles.questionsContainer}>
-            {DUMMY_QUESTIONS.map((quesion) => (
-              <Button
-                key={quesion}
-                text={quesion}
-                onClick={hanedleClickNameButton}
-              />
+            {DUMMY_QUESTIONS.map(question => (
+              <Button key={question.quesionId} text={question.question} onClick={hanedleClickNameButton} />
             ))}
           </div>
         </div>
