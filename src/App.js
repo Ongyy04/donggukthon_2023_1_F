e093459919ed0009cc1bc54474b006f17eaa58f7
Kong-E <kng001016@dgu.ac.kr>
2023-12-19 18:06:54 +0900

    Add Recoil

--- src/App.js
+++ src/App.js
@@ -1,61 +1,57 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import LoadingScreen from "./pages/InitialScreen";
-import Login from "./pages/Login";
-import Vote from "./pages/Vote";
-import Setting from "./pages/Setting";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Footer from "./components/Footer";
-import GroupMake from "./pages/GroupMake";
-import "./App.scss";
-import Waiting from "./pages/Waiting";
-import QuestionList from "./pages/QuestionList";
-import GroupList from "./pages/GroupList";
-import { QueryClient, QueryClientProvider } from "react-query";
-import QuestionResult from "./pages/QuestionResult";
-import Guess from "./pages/Guess";
-import GuessRightResult from "./pages/GuessRightResult";
-import GuessWrongResult from "./pages/GuessWrongResult";
-import InvitationCard from "./pages/InvitationCard";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import LoadingScreen from './pages/InitialScreen';
+import Login from './pages/Login';
+import Vote from './pages/Vote';
+import Setting from './pages/Setting';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import GroupMake from './pages/GroupMake';
+import './App.scss';
+import Waiting from './pages/Waiting';
+import QuestionList from './pages/QuestionList';
+import GroupList from './pages/GroupList';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import QuestionResult from './pages/QuestionResult';
+import Guess from './pages/Guess';
+import GuessRightResult from './pages/GuessRightResult';
+import GuessWrongResult from './pages/GuessWrongResult';
+import InvitationCard from './pages/InvitationCard';
+import { RecoilRoot } from 'recoil';
+
 const queryClient = new QueryClient();
 
 const AppContent = () => {
   const location = useLocation();
-  const renderHeaderAndFooter = location.pathname !== "/";
+  const renderHeaderAndFooter = location.pathname !== '/';
 
   return (
-    <div className={"mainDiv"}>
-      {/* "/" 경로가 아닐 때만 Header를 렌더링 */}
-      {renderHeaderAndFooter && <Header />}
-      <Routes>
-        <Route path="/" element={<LoadingScreen />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/groupMake" element={<GroupMake />} />
-        <Route path="/questionList" element={<QuestionList />} />
-        <Route path="/vote" element={<Vote />} />
-        <Route path="/waiting" element={<Waiting />} />
-        <Route path="/groupList" element={<GroupList />} />
-        <Route path="/questionResult" element={<QuestionResult />} />
-        <Route path="/guessRightResult" element={<GuessRightResult />} />
-        <Route path="/guessWrongResult" element={<GuessWrongResult />} />
-        <Route path="/guess" element={<Guess />} />
-        <Route path="/:roomName/vote/:memberID" element={<Vote />} />
-        <Route
-          path="invitation/:memberId/:groupId/"
-          element={<InvitationCard />}
-        />
+    <div className={'mainDiv'}>
+      <RecoilRoot>
+        {/* "/" 경로가 아닐 때만 Header를 렌더링 */}
+        {renderHeaderAndFooter && <Header />}
+        <Routes>
+          <Route path="/" element={<LoadingScreen />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/groupMake" element={<GroupMake />} />
+          <Route path="/questionList" element={<QuestionList />} />
+          <Route path="/vote" element={<Vote />} />
+          <Route path="/waiting" element={<Waiting />} />
+          <Route path="/groupList" element={<GroupList />} />
+          <Route path="/questionResult" element={<QuestionResult />} />
+          <Route path="/guessRightResult" element={<GuessRightResult />} />
+          <Route path="/guessWrongResult" element={<GuessWrongResult />} />
+          <Route path="/guess" element={<Guess />} />
+          <Route path="/:roomName/vote/:memberID" element={<Vote />} />
+          <Route path="invitation/:memberId/:groupId/" element={<InvitationCard />} />
 
-        <Route path="/setting" element={<Setting />} />
-        {/* ...other routes */}
-      </Routes>
-      {renderHeaderAndFooter && <Footer />}
+          <Route path="/setting" element={<Setting />} />
+          {/* ...other routes */}
+        </Routes>
+        {renderHeaderAndFooter && <Footer />}
+      </RecoilRoot>
     </div>
   );
 };
--- src/pages/GroupList.jsx
+++ src/pages/GroupList.jsx
@@ -1,38 +1,41 @@
-import React, { useState } from "react";
-import styles from "./Home.module.scss";
-import Button from "../components/Button";
-import { useQuery } from "react-query";
-import { getGroups } from "../api/group";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import styles from './Home.module.scss';
+import Button from '../components/Button';
+import { useQuery } from 'react-query';
+import { getGroups } from '../api/group';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../stores/user';
 
 const DUMMY_GROUPS = [
-  "동국대 헤커톤1",
-  "동국대 헤커톤2",
-  "동국대 헤커톤3",
-  "동국대 헤커톤4",
-  "동국대 헤커톤5",
-  "동국대 헤커톤6",
-  "동국대 헤커톤7",
-  "동국대 헤커톤8",
+  '동국대 헤커톤1',
+  '동국대 헤커톤2',
+  '동국대 헤커톤3',
+  '동국대 헤커톤4',
+  '동국대 헤커톤5',
+  '동국대 헤커톤6',
+  '동국대 헤커톤7',
+  '동국대 헤커톤8',
 ];
 
 const GroupList = () => {
   const navigator = useNavigate();
+  const user = useRecoilValue(userState);
   const [imageLoaded, setImageLoaded] = useState(true);
   const {
     data: groupsData,
     error,
     isLoading,
-  } = useQuery(["groups"], () => getGroups(1), {
-    select: (groupsData) => groupsData.data.groupList,
+  } = useQuery(['groups'], () => getGroups(user.memberId), {
+    select: groupsData => groupsData.data.groupList,
   }); // 임시로 1번 멤버의 그룹을 가져옴
 
   const handleImageLoad = () => {
     setImageLoaded(true);
   };
-  const hanedleClickQuestionButton = (e) => {
-    console.log("당신이 누른 질문은", e.target.textContent, "입니다.");
-    navigator("/questionList");
+  const hanedleClickQuestionButton = e => {
+    console.log('당신이 누른 질문은', e.target.textContent, '입니다.');
+    navigator('/questionList');
   };
 
   return (
@@ -44,13 +47,13 @@ const GroupList = () => {
               src="/assets/snow-character.png"
               alt="Decorative Snowflake"
               onLoad={handleImageLoad}
-              style={{ width: "200px", height: "195px" }}
+              style={{ width: '200px', height: '195px' }}
             />
           </div>
           <div className={styles.buttonsContainer}>
-            {DUMMY_GROUPS.map((group) => (
+            {DUMMY_GROUPS.map(group => (
               <Button text={group} onClick={hanedleClickQuestionButton} />
-            ))}{" "}
+            ))}{' '}
           </div>
         </div>
       ) : (
